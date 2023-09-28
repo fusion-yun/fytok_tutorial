248a38de04eebc14414eb57f50f4a49ab72a4489
Salmon <yuzhi@ipp.ac.cn>
2023-09-28 13:50:22 +0800

    [init]

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # fytok_tutorial
 
 #### 介绍
+![Image](docs/figures/FyTok.svg "FyTok")
+
 FyTok 教程，包含典型用例和插件模板。方便收集、反馈用户在使用过程的 issue。
 
 #### 软件架构
@@ -28,10 +30,4 @@ FyTok 教程，包含典型用例和插件模板。方便收集、反馈用户�
 
 
 #### 特技
-
-1.  使用 Readme\_XXX.md 来支持不同的语言，例如 Readme\_en.md, Readme\_zh.md
-2.  Gitee 官方博客 [blog.gitee.com](https://blog.gitee.com)
-3.  你可以 [https://gitee.com/explore](https://gitee.com/explore) 这个地址来了解 Gitee 上的优秀开源项目
-4.  [GVP](https://gitee.com/gvp) 全称是 Gitee 最有价值开源项目，是综合评定出的优秀开源项目
-5.  Gitee 官方提供的使用手册 [https://gitee.com/help](https://gitee.com/help)
-6.  Gitee 封面人物是一档用来展示 Gitee 会员风采的栏目 [https://gitee.com/gitee-stars/](https://gitee.com/gitee-stars/)
+ 
---
+++ setup.py
@@ -0,0 +1,50 @@
+from setuptools import find_namespace_packages, setup
+import subprocess
+
+
+version = (subprocess.check_output(["git", "describe", "--always", "--dirty"]).strip().decode("utf-8"))
+
+# Get the long description from the README file
+with open("README.md") as f:
+    long_description = f.read()
+
+with open("LICENSE.txt") as f:
+    license = f.read()
+
+# Get the requirements from the requirements.txt file
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+# Setup the package
+setup(
+    name="fytok_tutorial",
+
+    version=version,
+
+    description=f"Tutorial of FyTok {version}",
+
+    long_description=long_description,
+
+    url="https://gitee.com/openfusion/fytok_tutorial",
+
+    author="Zhi YU",
+
+    author_email="yuzhi@ipp.ac.cn",
+
+    license=license,
+
+    packages=find_namespace_packages(where="python"),  # 指定需要安装的包
+
+    package_dir={"": "python"},  # 指定包的root目录
+
+    classifiers=[
+        "Development Status :: 0 - Beta",
+        "Intended Audience :: Plasma Physicists",
+        "Topic :: Scientific/Engineering :: Physics",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords="plasma physics",  # 关键字列表
+    python_requires=">=3.10, <4",  # Python版本要求
+)
