caefd2e8ece3930957bf2e7e328b48f7c850287d
Salmon <yuzhi@ipp.ac.cn>
2023-12-02 20:30:09 +0800

    [working]

--- python/fytok/plugins/equilibrium/eq_demo.py
+++ python/fytok/plugins/equilibrium/eq_demo.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 import collections.abc
 import pathlib
 from fytok.modules.Magnetics import Magnetics
+from fytok.modules.TF import TF
+from fytok.modules.Wall import Wall
 from fytok.modules.PFActive import PFActive
 from fytok.modules.Equilibrium import Equilibrium
 from fytok.plugins.equilibrium.fy_eq import FyEqAnalyze
@@ -12,14 +14,35 @@ from spdm.utils.constants import *
 
 @Equilibrium.register(["eq_demo"])
 class EquilibriumDemo(FyEqAnalyze):
-    code = {"name": "eq_demo"}
+    code: Code = {"name": "eq_demo"}
 
     def execute(self, current: Equilibrium.TimeSlice, *previous, working_dir: pathlib.Path):
         super().execute(current, *previous, working_dir=working_dir)
+        tf: TF = self.inputs.get_source("tf")
+        wall: Wall = self.inputs.get_source("wall")
+        magnetics: Magnetics = self.inputs.get_source("magnetics")
+        pf_active: PFActive = self.inputs.get_source("pf_active")
         ####################
         # 在这里添加，调用外部程序代码
         # 工作目录为 working_dir
         #################
 
-    def refresh(self, *args, time=None, magnetics: Magnetics = None, pf_active: PFActive = None, **kwargs) -> None:
-        return super().refresh(*args, time=time, magnetics=magnetics, pf_active=pf_active, **kwargs)
+    def refresh(
+        self,
+        *args,
+        time=None,
+        wall: Wall = None,
+        tf: TF = None,
+        magnetics: Magnetics = None,
+        pf_active: PFActive = None,
+        **kwargs,
+    ) -> None:
+        return super().refresh(
+            *args,
+            time=time,
+            tf=tf,
+            wall=wall,
+            magnetics=magnetics,
+            pf_active=pf_active,
+            **kwargs,
+        )
