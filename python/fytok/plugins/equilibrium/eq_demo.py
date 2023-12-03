9021b12218335b8be41080f9ead8fe281c8529a1
Salmon <yuzhi@ipp.ac.cn>
2023-12-03 16:37:02 +0800

    [working]

--- python/fytok/plugins/equilibrium/eq_demo.py
+++ python/fytok/plugins/equilibrium/eq_demo.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-
+import os
 import collections.abc
 import pathlib
 from spdm.data.sp_property import sp_tree
@@ -8,18 +8,16 @@ from fytok.modules.TF import TF
 from fytok.modules.Wall import Wall
 from fytok.modules.PFActive import PFActive
 from fytok.modules.Equilibrium import Equilibrium
-
-
 from fytok.plugins.equilibrium.fy_eq import FyEqAnalyze
-
+from fytok.utils.logger import logger
 
 @Equilibrium.register(["eq_demo"])
 @sp_tree
 class EquilibriumDemo(FyEqAnalyze):
     code = {"name": "eq_demo", "copyright": "FyTok Demo"}
 
-    def execute(self, current: Equilibrium.TimeSlice, *previous, working_dir: pathlib.Path):
-        super().execute(current, *previous, working_dir=working_dir)
+    def execute(self, current: Equilibrium.TimeSlice, *previous,**kwargs):
+        super().execute(current, *previous,**kwargs)
         tf: TF = self.inputs.get_source("tf")
         wall: Wall = self.inputs.get_source("wall")
         magnetics: Magnetics = self.inputs.get_source("magnetics")
@@ -30,33 +28,17 @@ class EquilibriumDemo(FyEqAnalyze):
         # 工作目录为 working_dir
 
         with self.working_dir() as current_dir:
-            res = np.zeros([128, 128])
+
+            logger.debug((pathlib.Path.cwd(), current_dir))
+            # res = np.zeros([128, 128])
 
             time = current.time
 
             coil_current = [coil.current(time) for coil in pf_active.coil]
 
-            flux = [flux_loop.flux(time) for flux_loop in magnetics.flux_loop]
+            # flux = [flux_loop.flux(time) for flux_loop in magnetics.flux_loop]
 
         ####################
-        current.profiles_2d.psi = res
+        # current.update({ ... })
+        # current.profiles_2d.psi = ...
 
-    def refresh(
-        self,
-        *args,
-        time=None,
-        wall: Wall = None,
-        tf: TF = None,
-        magnetics: Magnetics = None,
-        pf_active: PFActive = None,
-        **kwargs,
-    ) -> None:
-        return super().refresh(
-            *args,
-            time=time,
-            tf=tf,
-            wall=wall,
-            magnetics=magnetics,
-            pf_active=pf_active,
-            **kwargs,
-        )
